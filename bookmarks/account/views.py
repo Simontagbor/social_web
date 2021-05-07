c313dad195909fb06ba466fd0a63737520d3ad32
Simontagbor <62922135+Simontagbor@users.noreply.github.com>
2021-05-07 21:01:44 +0000

    create user_login view. this view gets the username abd password from the user using the LoginForm. and then Authenticates the user against the database. snippet rewiew: instantiate the form if the user is submitting the name and password using rquest.POST. check if the form is valid. if it is authenticate the user using the authenticate method using the request object and the name and pasword retireved from the form.
    the authenticate method returns None or a user object if the user is authenticated.
    
    login the user:
    if user is not none and the user is active, use the login method to login the user, this method also uses the request object and the user object. otherwise display raw httpresponses  or ultimately instantiate a form for user to login.

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 # Create your views here.
+def user_login(request):
+    """a view to authenticate user against
+        database using username and password
+        retrieved from the user using LoginForm
+    """
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse('Authenticated Successfully!')
+            else:
+                return HttpResponse('Sorry this user is disabled')
+        else:
+            return HttpResponse('invalid login')
+    else:
+        form = LoginForm()
+
+    context = {'form': form,}
+    return render(request, 'account/login.html', context)
