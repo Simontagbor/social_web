8369bd7c61927ef8f893e0e679858dc9e1ddf046
Simontagbor <62922135+Simontagbor@users.noreply.github.com>
2021-05-12 20:00:32 +0000

    fix a bug in the url pattern for dashbord

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # Auth views
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
 
 ]
