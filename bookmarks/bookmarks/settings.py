a253509af9a2cfe5c56cd886be014356065b9962
Simontagbor <62922135+Simontagbor@users.noreply.github.com>
2021-05-13 16:10:16 +0000

    make django use the registration/logged_out.html of the account app instead of the admin app. make the account app come before the admin app in the installed_apps list in the settings.py file

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account.apps.AccountConfig',
+    'django.contrib.admin',
 ]
 
 MIDDLEWARE = [
