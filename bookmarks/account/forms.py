5b25f7d7c4a062451addaac6fba065041c3e072f
Simontagbor <62922135+Simontagbor@users.noreply.github.com>
2021-06-10 15:14:58 +0000

    Add UserEditForm and ProfileEditForm.
    UserEditForm: This will allow users to edit their first name, last name,
    and email, which are attributes of the built-in Django user model.
    
    ProfileEditForm: This will allow users to edit the profile data that you
    save in the custom Profile model. Users will be able to edit their date of
    birth and upload a picture for their profile.

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.forms import fields
 from django.forms.widgets import PasswordInput
-
+from .models import Profile
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
@@ -22,3 +23,13 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] !=  cd['password2']:
             raise forms.ValidationError('password don\'t match.')
         return cd['password2']
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model =  User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
